51753b3d5fda34205991ab0c02ba0c2f155139c5
Mikey8250 <mohitburnwal2@gmail.com>
2025-07-25 05:23:39 +0530

    Add cards layout to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,19 @@ function App() {
           </li>
         </ul>
       </nav>
+
+      <div className="w-[85%] p-5 pt-[5%] flex flex-wrap overflow-y-scroll">
+        <div className="mb-3 mr-3 CARD p-3 border shadow w-[18%] h-[30vh] flex flex-col justify-center items-center">
+          <div
+            className="w-full h-[80%] bg-contain bg-no-repeat bg-center mb-3 hover:scale-110"
+            style={{
+              backgroundImage:
+                'url("https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg")',
+            }}
+          ></div>
+          <h1 className="hover:text-blue-300">Lorem ipsum dolor sit amet.</h1>
+        </div>
+      </div>
     </div>
   );
 }
